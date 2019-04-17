bbb768699573f3cec2e95be276eac55d04d6b956
Aline Sotte <alinedpaso@gmail.com>
2019-04-16 23:05:16 -0300

    Modelo  Membros ONG

---
+++ src/main/java/br/ufjf/dcc193/trabalho1_dcc193/Model/Membros.java
@@ -0,0 +1,42 @@
+package br.ufjf.dcc193.trabalho1_dcc193.Model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import lombok.*;
+import org.hibernate.validator.constraints.Email;
+import org.springframework.data.annotation.Id;
+
+
+
+@Table(name="MEMBROS")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Membros {
+
+@Id
+@GeneratedValue(strategy = GenerationType.SEQUENCE)
+@Column(name="MEM_ID")
+private Long id;  
+
+@Column(name="MEM_FUNCAO")
+private String funcao;
+
+@Email
+@Column(name="MEM_EMAIL")
+private String email; 
+
+@Column(name="MEM_DATA_ENTIDADE")
+private Date dataEntrada;
+
+@Column(name="MEM_DATA_SAIDA")
+private Date dataSaida;
+
+
+
+}
