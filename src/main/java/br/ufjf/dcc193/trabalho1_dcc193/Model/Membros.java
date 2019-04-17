48786d49f4410e4f1e51873f40b4c296e9ac7339
Aline Sotte <alinedpaso@gmail.com>
2019-04-17 20:31:06 -0300

    Anotação @Email

--- src/main/java/br/ufjf/dcc193/trabalho1_dcc193/Model/Membros.java
+++ src/main/java/br/ufjf/dcc193/trabalho1_dcc193/Model/Membros.java
@@ -1,11 +1,14 @@
 package br.ufjf.dcc193.trabalho1_dcc193.Model;
 
 import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
 import lombok.*;
+import org.hibernate.validator.constraints.Email;
 import org.springframework.data.annotation.Id;
 
 
